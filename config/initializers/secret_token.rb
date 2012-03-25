a03398f94e1577fb5da7ebc86a9e446d4f9f8a52
Mario Herbers <mario_herbers@hotmail.com>
2012-03-25 21:07:45 +0200

    Added the RoR framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rewrite::Application.config.secret_token = 'a69e348c2371883ee098d30525cc712cacc71e6eaec82871c48ff87013b9164cbf4d52b6551f1d4b421f8c647a68b5a548f1d3c25df9d46c1bfc14b736c84da4'
